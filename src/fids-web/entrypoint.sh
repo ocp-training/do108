a9151d7367386395c4a6370a93355dcde99642f7
storbel <storbel@users.noreply.github.com>
2026-09-13 20:51:48 +0200

    Fix fids-web so OpenShift random UIDs can write index.html.

--- src/fids-web/entrypoint.sh
+++ src/fids-web/entrypoint.sh
@@ -9,9 +9,12 @@ escape() {
   printf '%s' "$1" | sed -e 's/[&|]/\\&/g'
 }
 
+tmp="$(mktemp)"
 sed -e "s|__POD_NAME__|$(escape "$pod")|g" \
     -e "s|__HOSTNAME__|$(escape "$host")|g" \
     -e "s|__API_BASE__|$(escape "$api")|g" \
-    /usr/share/fids/index.html > /var/www/html/index.html
+    /usr/share/fids/index.html > "$tmp"
+mv -f "$tmp" /var/www/html/index.html
+chmod a+r /var/www/html/index.html
 
 exec container-entrypoint "$@"
